3f961edc9b8b4363923a5c94ab458e1dafe2aff2
Dmitry Zoteev <122165438+DmitriyZoteev@users.noreply.github.com>
2023-03-03 17:50:10 +0500

    Delete src directory

--- src/main/java/web/spring_boot/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package web.spring_boot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.spring_boot.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  }
